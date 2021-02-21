e95628696dbdb2d5df6687017e3e57640485f2cd
semiuniversal <w.tremblay@gmail.com>
2021-02-21 15:56:10 -0500

    all working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import { ContactsProvider } from "../contexts/ContactsProvider";
 import { ConversationsProvider } from "../contexts/ConversationsProvider";
+import { SocketProvider } from "../contexts/SocketProvider";
 import useLocalStorage from "../hooks/useLocalStorage";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 
 function App() {
-  const [id, setId] = useLocalStorage("id");
-  const dashboard = (
-    <ContactsProvider>
-      <ConversationsProvider>
-        <Dashboard id={id} />
-      </ConversationsProvider>
-    </ContactsProvider>
-  );
-  return id ? dashboard : <Login onIdSubmit={setId} />;
+    const [id, setId] = useLocalStorage("id");
+    const dashboard = (
+        <SocketProvider id={id}>
+            <ContactsProvider>
+                <ConversationsProvider>
+                    <Dashboard id={id} />
+                </ConversationsProvider>
+            </ContactsProvider>
+        </SocketProvider>
+    );
+    return id ? dashboard : <Login onIdSubmit={setId} />;
 }
-
 export default App;
