f0a8e27886e66ebb53ea20c814219c4e46c1e5f4
semiuniversal <w.tremblay@gmail.com>
2021-02-20 21:47:07 -0500

    conversation sidebar working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { ContactsProvider } from "../contexts/ContactsProvider";
+import { ConversationsProvider } from "../contexts/ConversationsProvider";
 import useLocalStorage from "../hooks/useLocalStorage";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
@@ -8,7 +9,9 @@ function App() {
   const [id, setId] = useLocalStorage("id");
   const dashboard = (
     <ContactsProvider>
-      <Dashboard id={id} />
+      <ConversationsProvider>
+        <Dashboard id={id} />
+      </ConversationsProvider>
     </ContactsProvider>
   );
   return id ? dashboard : <Login onIdSubmit={setId} />;
